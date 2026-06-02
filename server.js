14f5cbfffa72a40f8b0763b15f7eb9bff6383efc
alburhanali-art <al.burhanali@gmail.com>
2026-06-02 19:30:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ app.post("/analyze", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`AI server running on port ${PORT}`);
 });
 
-});
